28665e79312d993dd5f058a0aed55744ee882dc1
Klaurie <k.laurie124@gmail.com>
2024-03-12 20:24:35 -0700

    chart generation tests

--- src/line/line.test.js
+++ src/line/line.test.js
@@ -5,34 +5,150 @@
 const fs = require("fs")
 const domTesting = require('@testing-library/dom')
 const { channel } = require("diagnostics_channel")
+const { TestEnvironment, default: JSDOMEnvironment } = require("jest-environment-jsdom")
+const { hasUncaughtExceptionCaptureCallback } = require("process")
+const { waitForDebugger } = require("inspector")
 require('@testing-library/jest-dom')
 const userEvent = require("@testing-library/user-event").default
+
 
 function initDomFromFiles(htmlPath, jsPath) {
 	const html = fs.readFileSync(htmlPath, 'utf8')
 	document.open()
 	document.write(html)
 	document.close()
-	jest.isolateModules(function() {
-		require(jsPath)
-	})
+	jest.resetModules()
+	require(jsPath)
 }
 
 describe('Invalid chart submission',  ()=> {
-    test('Create chart with empty fields error', async function() {
-        initDomFromFiles(`${__dirname}/line.html`, `${__dirname}/line.js`)
+  //  test('Create chart with empty fields error', async function() {
+    //    initDomFromFiles(`${__dirname}/line.html`, `${__dirname}/line.js`)
 		
-		const errorPopup = document.querySelector('.error-popup')
+		//const errorPopup = document.querySelector('.error-popup')
+		//const genChartButton = domTesting.getByText(document, 'Generate chart')
+		
+		//const user = userEvent.setup()
+//		await user.click(genChartButton)
+//		expect(errorPopup).not.toBeNull()
+//		expect(errorPopup.textContent).toBe('Error: No data specified!')
+//	})
+})
+
+describe('Chart Manipulation', ()=>{
+	test('Add more than one X, Y points', async function() {
+		initDomFromFiles(`${__dirname}/line.html`, `${__dirname}/line.js`)
+
+		// Assign first input with X, Y label
+		const xIn1 = domTesting.getByLabelText(document, 'X')
+		const yIn1 = domTesting.getAllByLabelText(document, 'Y')
+
+		// Add point button
+		const addButton = domTesting.getByText(document, '+')
+
+		// Set up user to click button
+		const user = userEvent.setup()
+		await user.click(addButton)
+
+		// UI should now contain two X and Y input boxes each
+		const inputsWithX = domTesting.getAllByLabelText(document, 'X')
+		const inputsWithY = domTesting.getAllByLabelText(document, 'Y')
+
+		// length of inputs for each need to be two
+		expect(inputsWithX.length).toEqual(2)
+		expect(inputsWithY.length).toEqual(2)
+
+		// An extra check to ensure the inputs are different elements
+		expect(xIn1).not.toBe(inputsWithX[1])
+		expect(yIn1).not.toBe(inputsWithY[1])
+	})
+	
+	test('Generate chart with point in correct location', async function(){
+		initDomFromFiles(`${__dirname}/line.html`, `${__dirname}/line.js`)
+		
+		jest.mock("../lib/generateChartImg.js")
+		const generateChartImgSpy = require("../lib/generateChartImg.js")
+
+		// Assign first input with X, Y label
+		const xIn1 = domTesting.getByLabelText(document, 'X')
+		const yIn1 = domTesting.getByLabelText(document, 'Y')
+
+		const xLabel = domTesting.getByLabelText(document, 'X label')
+		const yLabel = domTesting.getByLabelText(document, 'Y label')
+
+		const titleIn = domTesting.getByLabelText(document, 'Chart title')
+		const genChartButton = document.getElementById("generate-chart-btn");
+		
+		// User event sequence
+		const user = userEvent.setup()
+
+		// X and Y label inputs
+		await user.type(xLabel, 'X test')
+		await user.type(yLabel, 'Y test')
+
+		// Titel input
+		await user.type(titleIn, 'Test')
+
+		// X and Y inputs
+		await user.type(xIn1, '1')
+		await user.type(yIn1, '1')
+
+		// generate chart
+		await user.click(genChartButton)
+		
+		await domTesting.waitFor(() => {
+			expect(generateChartImgSpy).toHaveBeenCalledWith('line', [{ x: '1', y: '1' }], 'X test', 'Y test', 'Test', "#ff4500")
+			// get chart img
+			const chartDisp = domTesting.getByRole(document, 'img')
+			expect(chartDisp).toBeInTheDocument()
+		})
+
+		// Restore the spy
+		generateChartImgSpy.mockRestore();
+	})
+/*
+	test('Generate chart with point in correct location', async function(){
+		initDomFromFiles(`${__dirname}/line.html`, `${__dirname}/line.js`)
+
+		jest.mock("../lib/generateChartImg.js")
+		const generateChartImgSpy = require("../lib/generateChartImg.js")
+
+		// Assign first input with X, Y label
+		const xIn1 = domTesting.getByLabelText(document, 'X')
+		const yIn1 = domTesting.getByLabelText(document, 'Y')
+
+		const xLabel = domTesting.getByLabelText(document, 'X label')
+		const yLabel = domTesting.getByLabelText(document, 'Y label')
+
+		const titleIn = domTesting.getByLabelText(document, 'Chart title')
 		const genChartButton = domTesting.getByText(document, 'Generate chart')
 		
-<<<<<<< HEAD
-		const { document } = window;
-=======
->>>>>>> CICD-Integration
-
+		// User event sequence
 		const user = userEvent.setup()
-		await user.click(genChartButton)
-		expect(errorPopup).not.toBeNull()
-		expect(errorPopup.textContent).toBe('Error: No data specified!')
-	})
+
+		// X and Y label inputs
+		user.type(xLabel, 'X test')
+		user.type(yLabel, 'Y test')
+
+		// Titel input
+		user.type(titleIn, 'Test')
+
+		// X and Y inputs
+		user.type(xIn1, '1')
+		user.type(yIn1, '1')
+
+		// generate chart
+		user.click(genChartButton)
+		
+		// Wait for chart generation process to finish
+		await domTesting.waitFor(() => {
+			expect(generateChartImgSpy).toHaveBeenCalledWith('line', [{ x: '1', y: '1' }], 'X test', 'Y test', undefined, undefined)
+			
+			// get chart img
+			const chartDisp = domTesting.getByRole(document, 'img')
+			expect(chartDisp).toBeInTheDocument()
+		})
+		// Restore the spy
+		generateChartImgSpy.mockRestore();
+	}) */
 })
