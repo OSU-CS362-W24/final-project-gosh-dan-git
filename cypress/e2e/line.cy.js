8455507fcacde9bcdf4783be4b6032fb9ce0791b
Domanik Logan <domaniklogan2002@gmail.com>
2024-03-16 10:53:50 -0700

    revised end to end testing for bar, line, and scatter charts

--- cypress/e2e/line.cy.js
+++ cypress/e2e/line.cy.js
@@ -6,89 +6,24 @@ describe('Line.html basic operations', () => {
     })
   
     it('Should make a simple line chart', () => {
-        cy.get('#chart-title-input').type('My Line Chart')
-
-        cy.get('#x-label-input').type('x') 
-        cy.get('#y-label-input').type('y')
-        // x and y labels are in the data entry????
-        cy.get('.data-entry input').eq(2).type('1')
-        cy.get('.data-entry input').eq(3).type('2')
-        cy.get('#add-values-btn').click()
-        cy.get('.data-entry input').eq(4).type('3')
-        cy.get('.data-entry input').eq(5).type('4')
-        cy.get('#chart-color-input').click()
-        cy.get('#chart-color-input').eq(0).type('255')
-        cy.get('#generate-chart-btn').click()
-
-        cy.get('#chart-display').should('exist')
-    })
-
-    it('Should clear chart when buttom is pressed', () => {
-        cy.get('#chart-title-input').type('My Line Chart')
-        cy.get('#x-label-input').type('x') 
-        cy.get('#y-label-input').type('y')
-        cy.get('.data-entry input').eq(2).type('1')
-        cy.get('.data-entry input').eq(3).type('2')
-        cy.get('#add-values-btn').click()
-        cy.get('.data-entry input').eq(4).type('3')
-        cy.get('.data-entry input').eq(5).type('4')
-        cy.get('#generate-chart-btn').click()
-
-        cy.get('#chart-display').should('exist')
-        cy.get('#clear-chart-btn').click()
-
-        // should check if chart is empty
-        cy.get('#chart-title-input').should('have.value', '');
-        cy.get('#x-label-input').should('have.value', '');
-        cy.get('#y-label-input').should('have.value', '');
-        cy.get('.data-entry input').eq(2).should('have.value', '');
-        cy.get('.data-entry input').eq(3).should('have.value', '');
-    })
-
-    it('Should save the chart, and should be viewable in Gallery', () => {
-        cy.get('#chart-title-input').type('My Line Chart')
-        cy.get('#x-label-input').type('x') 
-        cy.get('#y-label-input').type('y')
-        cy.get('.data-entry input').eq(2).type('1')
-        cy.get('.data-entry input').eq(3).type('2')
-        cy.get('#add-values-btn').click()
-        cy.get('.data-entry input').eq(4).type('3')
-        cy.get('.data-entry input').eq(5).type('4')
-        cy.get('#chart-color-input').invoke('val', '#ff0000').trigger('input');
-        cy.get('#generate-chart-btn').click()
-        cy.get('#chart-display').should('exist')
-
+        cy.makeChart()
         cy.get('#save-chart-btn').click()
         cy.get('a[href="./"]').click();
-        cy.get('.chart-card').should('have.length', 1);
-        
-    })
-
-    it('Should take you back to the line chart of the saved image upon click', () => {
-        cy.get('#chart-title-input').type('My Line Chart')
-        cy.get('#x-label-input').type('x') 
-        cy.get('#y-label-input').type('y')
-        cy.get('.data-entry input').eq(2).type('1')
-        cy.get('.data-entry input').eq(3).type('2')
-        cy.get('#add-values-btn').click()
-        cy.get('.data-entry input').eq(4).type('3')
-        cy.get('.data-entry input').eq(5).type('4')
-        cy.get('#chart-color-input').invoke('val', '#ff0000').trigger('input');
-        cy.get('#generate-chart-btn').click()
-        cy.get('#chart-display').should('exist')
-
-        cy.get('#save-chart-btn').click()
-        cy.get('a[href="./"]').click();
+        cy.url().should('eq', 'http://localhost:8080/')
         cy.get('.chart-card').should('have.length', 1);
         cy.get('.chart-card').eq(0).click()
         cy.url().should('include', '/line.html');
-        cy.get('#chart-title-input').should('have.value', 'My Line Chart');
-        cy.get('#x-label-input').should('have.value', 'x');
-        cy.get('#y-label-input').should('have.value', 'y');
-        cy.get('.data-entry input').eq(2).should('have.value', '1');
-        cy.get('.data-entry input').eq(3).should('have.value', '2');
-        cy.get('.data-entry input').eq(4).should('have.value', '3');
-        cy.get('.data-entry input').eq(5).should('have.value', '4');
+        cy.get('#chart-title-input').should('have.value', 'My Chart');
+        cy.get('#chart-title-input').type('My Line Chart');
+        cy.get('#add-values-btn').click()
+        cy.get('.data-entry input').eq(6).type('5')
+        cy.get('.data-entry input').eq(7).type('6')
+        cy.get('#generate-chart-btn').click()
+        cy.get('#save-chart-btn').click()
+        cy.get('a[href="./"]').click();
+        cy.url().should('eq', 'http://localhost:8080/')
+        cy.get('.chart-card').should('have.length', 1);
+
         
     })
 
@@ -100,15 +35,8 @@ describe('Line.html basic operations', () => {
     })
 
     it('Should allow saving multiple charts', () => {
-        // Test to ensure multiple charts can be saved and viewed in the Gallery
-        cy.get('#chart-title-input').type('My Line Chart 1')
-        cy.get('#x-label-input').type('x') 
-        cy.get('#y-label-input').type('y')
-        cy.get('.data-entry input').eq(2).type('1')
-        cy.get('.data-entry input').eq(3).type('2')
-        cy.get('#add-values-btn').click()
-        cy.get('.data-entry input').eq(4).type('3')
-        cy.get('.data-entry input').eq(5).type('4')
+        cy.makeChart()
+        cy.get('#save-chart-btn').click()
         cy.get('#generate-chart-btn').click()
         cy.get('#save-chart-btn').click()
         cy.get('#chart-title-input').clear().type('My Line Chart 2')
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -9,6 +9,21 @@
 // ***********************************************
 //
 //
+Cypress.Commands.add("makeChart", () => {
+    cy.get('#chart-title-input').type('My Chart');
+    cy.get('#x-label-input').type('x');
+    cy.get('#y-label-input').type('y');
+    cy.get('.data-entry input').eq(2).type('1');
+    cy.get('.data-entry input').eq(3).type('2');
+    cy.get('#add-values-btn').click();
+    cy.get('.data-entry input').eq(4).type('3');
+    cy.get('.data-entry input').eq(5).type('4');
+    cy.get('#chart-color-input').click();
+    cy.get('#chart-color-input').eq(0).type('255');
+    cy.get('#generate-chart-btn').click();
+    cy.get('#chart-display').should('exist');
+  });
+  
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
 //
