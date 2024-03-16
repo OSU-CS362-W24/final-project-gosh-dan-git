1e12ed28ac85af5003c32a657c3d408e7d20e875
Steve-Akiyama <saakiyama02@gmail.com>
2024-03-16 07:31:25 -0700

    Updated and completed sortPoints tests
    Added simple tests for 10 object lists, both ordered an unordered

--- src/lib/sortPoints.test.js
+++ src/lib/sortPoints.test.js
@@ -2,9 +2,21 @@ const sortPoints = require('../lib/sortPoints')
 
 
 test('Unordered 2 object list', () => {
-    expect(sortPoints([{x: 5, y: 2}, {x: 3, y: 4}])).toStrictEqual([{x: 3, y: 4}, {x: 5, y: 2}])
+    expect(sortPoints([{x: 5, y: 2}, {x: 3, y: 4}]))
+    .toStrictEqual([{x: 3, y: 4}, {x: 5, y: 2}])
 })
     
-test('Unordered 2 object list', () => {
-    expect(sortPoints([{x: 3, y: 4}, {x: 5, y: 2}])).toStrictEqual([{x: 3, y: 4}, {x: 5, y: 2}])
+test('Ordered 2 object list', () => {
+    expect(sortPoints([{x: 3, y: 4}, {x: 5, y: 2}]))
+    .toStrictEqual([{x: 3, y: 4}, {x: 5, y: 2}])
+})
+
+test('Ordered 10 object list', () => {
+    expect(sortPoints([{x: -30, y: 47}, {x: -10, y: -12}, {x: -5, y: 1}, {x: 0, y: 6}, {x: 3, y: 4}, {x: 5, y: 2}, {x: 10, y: 3}, {x: 9999, y: -9999}, {x: 10000, y: 0}, {x: 12345, y: 54321}]))
+    .toStrictEqual([{x: -30, y: 47}, {x: -10, y: -12}, {x: -5, y: 1}, {x: 0, y: 6}, {x: 3, y: 4}, {x: 5, y: 2}, {x: 10, y: 3}, {x: 9999, y: -9999}, {x: 10000, y: 0}, {x: 12345, y: 54321}])
+})
+
+test('Unordered 10 object list', () => {
+    expect(sortPoints([{x: 12345, y: 54321}, {x: 10, y: 3}, {x: 9999, y: -9999}, {x: -10, y: -12}, {x: 0, y: 6}, {x: 3, y: 4}, {x: 10000, y: 0}, {x: -30, y: 47}, {x: -5, y: 1}, {x: 5, y: 2}]))
+    .toStrictEqual([{x: -30, y: 47}, {x: -10, y: -12}, {x: -5, y: 1}, {x: 0, y: 6}, {x: 3, y: 4}, {x: 5, y: 2}, {x: 10, y: 3}, {x: 9999, y: -9999}, {x: 10000, y: 0}, {x: 12345, y: 54321}])
 })
